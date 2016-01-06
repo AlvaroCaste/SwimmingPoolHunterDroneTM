d8afa15fa2c21224786e2a4850ae65bd7fa78ae1
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2016-01-06 15:54:04 +0100

    Add another test when range 1

--- src/test/scala/SwimmingPoolHunterDroneTM/HunterTest.scala
+++ src/test/scala/SwimmingPoolHunterDroneTM/HunterTest.scala
@@ -12,4 +12,9 @@ class HunterTest extends FlatSpec with ShouldMatchers {
     Hunter.getAllResidentialAreaInARange(38.56889, 40.511107, 1) shouldBe
     Set(7, 8, 9, 12, 14, 17, 18, 19)
   }
+
+  it should "have 1, 2, 3, 8, 13, 12, 11, 6 ids when coords are 28.56889, 45.511107 and range is 1" in {
+    Hunter.getAllResidentialAreaInARange(28.56889, 45.511107, 1) shouldBe
+    Set(1, 2, 3, 8, 13, 12, 11, 6)
+  }
 }
