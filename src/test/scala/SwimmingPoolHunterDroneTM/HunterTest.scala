86dc1cb682f827c169dce87949f9720bbb68fa12
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2016-01-06 14:11:04 +0100

    Add test when range 1

--- src/test/scala/SwimmingPoolHunterDroneTM/HunterTest.scala
+++ src/test/scala/SwimmingPoolHunterDroneTM/HunterTest.scala
@@ -5,6 +5,11 @@ import org.scalatest.{ShouldMatchers, FlatSpec}
 class HunterTest extends FlatSpec with ShouldMatchers {
 
   "Residential area list" should "be empty when range is 0" in {
-    Hunter.getAllResidentialAreaInARange(38.56889, 40.511107, 0) shouldBe List()
+    Hunter.getAllResidentialAreaInARange(38.56889, 40.511107, 0) shouldBe Set()
+  }
+
+  it should "have 7, 8, 9, 12, 14, 17, 18 ids when coords are 38.56889, 40.511107 and range is 1" in {
+    Hunter.getAllResidentialAreaInARange(38.56889, 40.511107, 1) shouldBe
+    Set(7, 8, 9, 12, 14, 17, 18, 19)
   }
 }
